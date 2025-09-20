627f35fb21d84405fef34df59e9d7d1ba2b6ad9b
Fay Fang <127782344+fayfang2@users.noreply.github.com>
2025-09-20 17:19:25 +0100

    Fix norminette on chunk_utils.

--- src/chunks_utils.c
+++ src/chunks_utils.c
@@ -6,7 +6,7 @@
 /*   By: fayfang <fayfang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/13 15:39:05 by fayfang           #+#    #+#             */
-/*   Updated: 2025/09/20 17:05:46 by fayfang          ###   ########.fr       */
+/*   Updated: 2025/09/20 17:19:07 by fayfang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,10 @@ t_queue	*set_stack(t_chunk *chunk, t_queue *queue)
 {
 	t_queue	*stack;
 
-	stack = (chunk->flag == queue->flag) ? queue : queue->other;
+	if (chunk->flag == queue->flag)
+		stack = queue;
+	else
+		stack = queue->other;
 	return (stack);
 }
 
@@ -36,7 +39,7 @@ void	set_loc(t_chunk *chunk)
 	chunk->min->loc = bottom;
 	if (chunk->flag == b && chunk->loc == bottom)
 		chunk->min->loc = top;
-	chunk->mid->loc = bottom; 
+	chunk->mid->loc = bottom;
 	if (chunk->flag == a)
 		chunk->mid->loc = top;
 	chunk->max->loc = top;
@@ -47,32 +50,20 @@ void	set_loc(t_chunk *chunk)
 void	set_pivots(t_chunk *chunk)
 {
 	long	div;
-	// long	mod;
-    // static int iteration = 0;
-    // iteration++;
-	
+
 	div = chunk->size / 3;
-	// mod = chunk->size % 3;
 	chunk->pivot1 = chunk->lower + div;
 	chunk->pivot2 = chunk->lower + div * 2;
-/* 	if (mod == 2)
-	{
-		chunk->pivot1 += 1;
-		chunk->pivot2 += 2;
-	} */
 	chunk->min->lower = chunk->lower;
 	chunk->mid->lower = chunk->pivot1;
 	chunk->max->lower = chunk->pivot2;
-/* 	printf("Debug iterations: %d\n", iteration);
-	printf("loc: %d, flag: %c, size: %ld\n", chunk->loc, chunk->flag, chunk->size);
-	printf("lower: %ld\t pivot1: %ld\t pivot2: %ld\t upper: %ld\n", chunk->lower, chunk->pivot1, chunk->pivot2, chunk->lower + chunk->size - 1); */
 }
 
 void	set_head(t_chunk *chunk, t_queue *queue)
 {
-	t_queue *stack;
+	t_queue	*stack;
 
-	stack = (chunk->flag == queue->flag) ? queue : queue->other;
+	stack = set_stack(chunk, queue);
 	if (chunk->loc == top)
 		chunk->head = stack->head;
 	else if (chunk->loc == bottom)
